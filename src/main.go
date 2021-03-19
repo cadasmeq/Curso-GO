8a0180701d4207f5b089e1456f182350be15a299
cristopher adasme <cadasmeq@gmail.com>
2021-03-19 10:41:04 -0300

    Fmt package

--- src/main.go
+++ src/main.go
@@ -1,27 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Números enteros
-	// int = Depende del OS (x32, x64)
-	// int8 = -128 a 127
-	// int16 = -2^15 a 2^15-1
-	// int32 = -2^31 a 2^31-1
-	// int64 = -2^63 a 2^63-1
+	helloMessage := "hello"
+	worldMessage := "world"
 
-	// Números enteros positivos
-	// uint = Depende del OS (x32, x64)
-	// uint8 = 0 a 2^8-1
-	// uint16 =0 a 2^16-1
-	// uint32 =0 a 2^32-1
-	// uint64 =0 a 2^65-1
+	fmt.Println(helloMessage, worldMessage)
+	fmt.Println(helloMessage, worldMessage)
 
-	// Decimales
-	// float32 = 32bits = +/- 1.18e^-38 a +/- 3.4e^38
-	// float64 = 64bits = +/- 2.23e^-38 a +/- 1.8e^38
+	nombre := "Platzi"
+	cursos := 500
+	fmt.Printf("%s tiene más de %d cursos\n", nombre, cursos)
+	fmt.Printf("%v tiene más de %v cursos\n", nombre, cursos)
 
-	// Números Complejos
-	// Complex64 = Real e imaginario float32
-	// Clomplex128 = Real e imaginario float64
-	// c := 10 + 8i
+	message := fmt.Sprintf("%s tiene más de %d cursos", nombre, cursos)
+	fmt.Println(message)
 
+	// Tipo de Dato
+	fmt.Printf("helloMessage : %T\n", helloMessage)
+	fmt.Printf("cursos : %T", cursos)
 }
