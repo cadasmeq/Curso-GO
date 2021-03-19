3f030905cd9008adfe9919551552026f6a66a2e3
cristopher adasme <cadasmeq@gmail.com>
2021-03-19 10:33:56 -0300

    primitivos

--- src/main.go
+++ src/main.go
@@ -1,43 +1,27 @@
 package main
 
-import "fmt"
-
 func main() {
-	// Area cuadrado
-	const baseCuadrado = 10
-	areaCuadrado := baseCuadrado * baseCuadrado
+	// Números enteros
+	// int = Depende del OS (x32, x64)
+	// int8 = -128 a 127
+	// int16 = -2^15 a 2^15-1
+	// int32 = -2^31 a 2^31-1
+	// int64 = -2^63 a 2^63-1
 
-	fmt.Println(areaCuadrado)
+	// Números enteros positivos
+	// uint = Depende del OS (x32, x64)
+	// uint8 = 0 a 2^8-1
+	// uint16 =0 a 2^16-1
+	// uint32 =0 a 2^32-1
+	// uint64 =0 a 2^65-1
 
-	x := 10
-	y := 50
+	// Decimales
+	// float32 = 32bits = +/- 1.18e^-38 a +/- 3.4e^38
+	// float64 = 64bits = +/- 2.23e^-38 a +/- 1.8e^38
 
-	// Suma
-	result := x + y
-	fmt.Println(result)
-
-	// Resta
-	result = y - x
-	fmt.Println(result)
-
-	// Multiplicación
-	result = x * y
-	fmt.Println(result)
-
-	// División
-	result = y / x
-	fmt.Println(result)
-
-	// Modulo
-	result = y % x
-	fmt.Println(result)
-
-	// Incremental
-	x++
-	fmt.Println(x)
-
-	// Decremental
-	y--
-	fmt.Println(y)
+	// Números Complejos
+	// Complex64 = Real e imaginario float32
+	// Clomplex128 = Real e imaginario float64
+	// c := 10 + 8i
 
 }
