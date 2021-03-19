83afa3e56790093d20a1eb48350d8075aa81d7dd
cristopher adasme <cadasmeq@gmail.com>
2021-03-19 17:50:08 -0300

    Array, slice and slicing

--- src/main.go
+++ src/main.go
@@ -3,24 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	// Defer
-	defer fmt.Println("hola")
-	fmt.Println("mundo")
+	// Array
+	var array [5]int
+	array[0] = 1
+	array[1] = 2
 
-	// Continue y break
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	fmt.Println(len(array), cap(array))
 
-		// Continue
-		if i == 2 {
-			fmt.Println("Es 2")
-			continue
-		}
+	// Slice
+	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println(slice, len(slice), cap(slice))
 
-		// Break
-		if i == 6 {
-			fmt.Println("break")
-		}
-	}
+	// Sliceing
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
+
+	// Append
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
 
 }
