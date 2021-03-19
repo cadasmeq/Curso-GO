e1280242adde36795d6b1eb281251890a2614695
cristopher adasme <cadasmeq@gmail.com>
2021-03-19 17:28:29 -0300

    if sentences

--- src/main.go
+++ src/main.go
@@ -2,22 +2,6 @@ package main
 
 import "fmt"
 
-func Printer(message string) {
-	fmt.Println(message)
-}
-
-func tripleArgument(a, b int, c string) {
-	fmt.Println(a, b, c)
-}
-
-func returnValue(a int) int {
-	return a * 2
-}
-
-func doubleReturn(a int) (c, d int) {
-	return a, a * 2
-}
-
 func main() {
 
 	// For
