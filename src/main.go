e5313aac9ac051964fcf610b687652d660ed37e5
cris <cadasmeq@gmail.com>
2021-03-20 17:27:46 -0300

    Channels

--- src/main.go
+++ src/main.go
@@ -1,38 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "fmt"
 
-func say(text string, wg *sync.WaitGroup) {
-	// Libera la gorutine del wait group
-	defer wg.Done()
-	println(text)
+func inChannel(text string, c chan<- string) {
+	c <- text
+}
+
+func outChannel(text string, c <-chan string) {
+	text = <-c
+	fmt.Println(text)
 }
 
 func main() {
-	// Declarar wait group
-	// El cual va acumulando un grupo de gorutines
-	// y las va liberando poco a poco
-	var wg sync.WaitGroup
+	c := make(chan string, 1)
 
-	fmt.Println("hello")
+	go inChannel("hello", c)
+	fmt.Println(<-c)
 
-	// Agregar una gorutine al waitgroup
-	wg.Add(2)
-	go say("world", &wg)
-	go say("again", &wg)
+	fmt.Println("world")
 
-	// Indica a la gorutine principal (wait) que espere
-	// hasta que el waitgroup finalice con sus gorutines
-	wg.Wait()
-
-	// Función anonima que tiene vida solo dentro de si misma
-	go func(text string) {
-		fmt.Println(text)
-	}("Adios")
-
-	time.Sleep(time.Second * 1)
 }
